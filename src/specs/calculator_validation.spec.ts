78bb086a2380edd825f5d28d3d6780ecff7d2dbe
bayek0fsiwa <imvikas@tutanota.com>
2026-03-22 22:19:05 +0530

    fix negative scenario assertion

--- src/specs/calculator_validation.spec.ts
+++ src/specs/calculator_validation.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from '@wdio/globals';
 import CalculatorPage from '../pages/calculator.page';
+import { sampleData } from '../data/retirementData';
 
 describe('Retirement Calculator Validation (Negative Testing)', () => {
 
@@ -29,10 +30,15 @@ describe('Retirement Calculator Validation (Negative Testing)', () => {
         await expect(CalculatorPage.errorSavingsIncreaseRate).toHaveText('Input required');
     });
 
-    // it('should show specific error when retirement age is less than or equal to current age', async () => {
-    //     await CalculatorPage.setInputValue(CalculatorPage.inputCurrentAge, 45);
-    //     await CalculatorPage.setInputValue(CalculatorPage.inputRetirementAge, 40);
-    //     await CalculatorPage.clickElement(CalculatorPage.btnCalculate);
-    //     await expect(CalculatorPage.errorRetirementAge).toBeDisplayed();
-    // });
+    it('should show specific error when retirement age is less than or equal to current age', async () => {
+        await CalculatorPage.setInputValue(CalculatorPage.inputCurrentAge, 45);
+        await CalculatorPage.setInputValue(CalculatorPage.inputRetirementAge, 40);
+        await CalculatorPage.setMaskedValue(CalculatorPage.inputCurrentIncome, sampleData.currentAnnualIncome);
+        await CalculatorPage.setMaskedValue(CalculatorPage.inputCurrentSavings, sampleData.currentRetirementSavings);
+        await CalculatorPage.setMaskedValue(CalculatorPage.inputAnnualSavings, sampleData.currentRetirementContribution);
+        await CalculatorPage.setMaskedValue(CalculatorPage.inputSavingsIncreaseRate, sampleData.annualContributionIncrease);
+        await CalculatorPage.clickElement(CalculatorPage.btnCalculate);
+        await expect(CalculatorPage.errorRetirementAge).toBeDisplayed();
+        await expect(CalculatorPage.errorRetirementAge).toHaveText('Planned retirement age must be greater than current age');
+    });
 });
